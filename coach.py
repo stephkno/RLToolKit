d456e4d352518b9af8b106c5d596acb1c8958418
Stephen <stephkno@gmail.com>
2019-10-23 19:25:40 -0700

    Update coach.py

--- coach.py
+++ coach.py
@@ -1,18 +1,21 @@
 import gym
-
+#
+# Reinforcement Learning Coach
+#
 class Coach():
-    def __init__(self, env, n):
+    def __init__(self, env, target_agent, loss_fn, lr, optim, n_agents):
         super(Coach, self).__init__()
         self.name = env
         self.envs = []
-        self.n = n
-        self.dones = [False for _ in range(n)]
-        self.memory = []
+        self.n_agents = n_agents
+        self.dones = [False for _ in range(n_agents)]
         self.length = 0
 
-        for _ in range(self.n):
+        #create environments
+        for _ in range(self.n_agents):
             self.envs.append(gym.make(self.name))
 
+    #reset all environments
     def reset(self):
         states = []
 
@@ -21,9 +24,29 @@ class Coach():
 
         return states
 
+    #agent takes a step
     def step(self, actions):
-        return [x.step(actions[i]) for i,x in enumerate(self.envs)]
+        obs = []
+        rewards = []
+        dones = []
+        infos = []
 
-    def push(self, sample):
-        memory.append(sample)
-        self.length += 1
+        #take step for each agent/environment
+        for i,env in enumerate(self.envs):
+            obv, reward, done, info = env.step(actions[i])
+            obs.append(obv)
+            rewards.append(reward)
+            dones.append(done)
+            infos.append(info)
+
+        #send each return as separate vecs
+        return obs, rewards, dones, infos
+
+    def reinforce(self, gamma):
+        pass
+
+    def run_episode(self, return_tau=False, render=False):
+        pass
+
+    def train(self, gamma=0.99, epochs=10):
+        pass
