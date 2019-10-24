c4e41e8d3889f4c05a305586bcfd4f811d3d0814
Stephen <stephkno@gmail.com>
2019-10-24 00:13:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
-import models
-from utils import Coach
+#!/Users/stephen/miniconda3/bin/python
+from coach import Coach
+from controllers import Agent_Controller
+from agents import RandomAgent
 import torch
 import gym
 import boardgame2
 import torch
 import numpy
 
-discount = 1.0
+discount = 0.99
 batch_size = 64
-n_agents = 3
+n_agents = 2
 n_episodes = 10
 n_epochs = 100
 rate = 0.0001
 
-agent = models.Agent_Controller(
-            batch=batch_size,
-            agents=n_agents,
-            model=torch.nn.Sequential(
-                    torch.nn.Linear(4, 128),
-                    torch.nn.Tanh(),
-                    torch.nn.Linear(128, 3)
-            ),
-            agent_type=models.Softmax_Agent
-            )
-
 coach = Coach(env="CartPole-v0",
-              target_agent=agent,
               loss_fn=Coach.reinforce,
               lr=rate,
               optim=torch.optim.Adam,
               n_agents=n_agents
               )
 
+target_agent = RandomAgent(actions=coach.actions)
+
+agent = Agent_Controller(
+            batch=batch_size,
+            n_agents=n_agents,
+            agent=target_agent
+            )
+
 for epoch in range(n_epochs):
     for episode in range(n_episodes):
-        returns = coach.run_episode(return_tau=True, render=False)
+        returns = coach.run_episode(agent, return_tau=True, render=True)
         print(returns)
 
     loss = coach.train(gamma=discount, epochs=n_epochs)
