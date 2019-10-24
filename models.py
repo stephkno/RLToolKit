2e6979db2fd0354b48672117237d23e3acffa785
Stephen <stephkno@gmail.com>
2019-10-23 19:25:27 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,22 +1,46 @@
 import torch
+import copy
 
-class Actor():
-    def __init__(self, optim, lr, agents, gamma, batch, model):
-        super(Actor, self).__init__()
-        self.optimizer = optim(lr=lr, params=self.parameters())
+#used for neural episodic control
+class Episodic_Controller():
+    def __init__(self):
+        super(Episodic_Controller, self).__init__()
+        self.brain = {}
+
+    def read(self, x):
+        pass
+
+    def write(self, x):
+        pass
+
+
+#used for training agents with pure gradient methods
+class Agent_Controller():
+    def __init__(self, agents, batch, model, agent_type):
+        super(Agent_Controller, self).__init__()
         self.model = model
-        self.gamma = gamma
         self.batch = batch
         self.n_agents = agents
-        self.agents = [Network(model) for _ in range(self.n_agents)]
+        self.agent_type = agent_type
+        self.initialize_agents()
 
-class Network(torch.nn.Module):
+    def initialize_agents(self):
+        self.target_agent = self.agent_type(self.model)
+        self.agents = [copy.copy(self.target_agent) for _ in range(self.n_agents)]
+
+    def step(self, x):
+        return [agent(x) for agent in self.agents]
+
+class Softmax_Agent(torch.nn.Module):
     def __init__(self, model):
-        super(Actor, self).__init__()
+        super(Softmax_Agent, self).__init__()
         self.model = model
-        self.activation = torch.nn.Softmax(dim=-1)
+        self.memory = []
+
+    def push(self, transition):
+        self.memory.push(transition)
 
     def forward(self, x):
         y = self.model(x)
-        y = self.activation(y)
+        y = torch.nn.Softmax(dim=-1)(y)
         return y
