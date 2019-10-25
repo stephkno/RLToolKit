db2061ed5125ba7bedfd18f9a0f74b8c42054c14
Stephen <stephkno@gmail.com>
2019-10-25 03:01:53 -0700

    Update controllers.py

--- controllers.py
+++ controllers.py
@@ -1,18 +1,61 @@
 import gym
-import torch
+import numpy as np
 import copy
+import random
+import torch
+import time
 
 #used for neural episodic control
+#
+#todo: implement episode replay
+#todo: implement update NEC w/ belleman
+#todo: implement neural trainer
+#
 class Episodic_Controller():
-    def __init__(self):
+    def __init__(self, k, n_actions=0):
         super(Episodic_Controller, self).__init__()
+        if n_actions <= 1:
+            raise Exception("Needs more than 1 action")
+
         self.brain = {}
+        self.states = 0
+        self.k = k
+        self.actions = n_actions
 
     def read(self, x):
-        pass
+        keys = self.nearest_neighbors(x)
+        actions = torch.tensor([self.brain[key[1]] for key in keys]).mean(dim=0)
+        return actions
 
-    def write(self, x):
-        pass
+    def write(self, obv):
+        obv = obv.squeeze(0)
+        #initialize state pair
+        #start zero or random values?
+        self.brain[obv] = np.zeros(self.actions)
+
+    def nearest_neighbors(self, obv):
+        keys = list(self.brain.keys())
+        #diff = torch.nn.functional.cosine_similarity(keys, obv, dim=1, eps=1e-8)?
+        diff = torch.abs(obv-torch.stack(keys)).sum(dim=1)
+        keys = list(zip(list(diff), keys))
+        keys.sort(key=lambda value: value[0])
+        return keys[:self.k]
+
+    #agent takes a step
+    #reading and finding nearest neighbors
+    #taking action, env step, return rewards
+    def step(self, obv):
+        obv = torch.tensor(obv)
+        if obv not in self.brain:
+            if len(list(self.brain.keys())) < self.k:
+                action = [random.choice(list(range(self.actions)))]
+            else:
+                action = self.read(obv)
+                action = [int(torch.argmax(action))]
+            #write new obervation only after kNN search
+            self.write(obv)
+
+        return action
 
 #used for training agents with pure gradient methods
 class Agent_Controller():
