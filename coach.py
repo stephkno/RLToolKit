9f581ca5ce6a1ff7311ab93976c5ae110c33c1a5
Stephen <stephkno@gmail.com>
2019-11-08 21:52:16 -0800

    Update coach.py

--- coach.py
+++ coach.py
@@ -1,11 +1,16 @@
 import gym
+import gym_minigrid
 import datetime
 import numpy as np
 import torch
+import random
 import cv2
 import pygame
 from memory import Memory
 from embedder import Embedder
+import os
+import copy
+import atexit
 pygame.init()
 
 ## Reinforcement Learning Coach
@@ -13,15 +18,22 @@ pygame.init()
 ### Handles OpenAI Gym environments
 #
 class Coach():
-    def __init__(self, env, loss_fn, optim, n_agents, embed=False, flatten=False):
+    def __init__(self, env, loss_fn, optim, n_agents, gym=gym, preprocess_func=None, utility_func=None, embed=False, flatten=False, frameskip=1, batch_size=32):
 
+        if utility_func == None:
+            raise Exception("Need utility function")
+        if preprocess_func == None:
+            raise Exception("Need preprocess function")
         if n_agents < 1:
             raise Exception("Need at least 1 agent.")
         if n_agents > 1:
             raise NotImplementedError
 
         super(Coach, self).__init__()
+        print("Initializing Coach")
+
         self.name = env
+        self.gym = gym
         self.n_agents = n_agents
         self.dones = [False for _ in range(n_agents)]
         self.length = 0
@@ -33,33 +45,55 @@ class Coach():
         self.best_episode = []
         self.p_obs = torch.zeros(1)
         self.values = []
-        self.frameskip = 1
+        self.frameskip = frameskip
         self.loss_fn=loss_fn
         self.optim = optim
         self.preview = False
-        self.norm = False
+        self.norm = True
         self.render = False
+        self.caution = 0.9
+        self.life = -1
+        self.life = -1
+        self.init_skip = 70
+        self.render_frameskip = 1
+        self.mid_skip = 0
+        self.steps = 0
+        self.batch_size = batch_size
+        self.preprocess_frame = preprocess_func
+        self.update = utility_func
 
+        atexit.register(self.end)
         #create environments
-        self.envs = [gym.make(self.name) for _ in range(self.n_agents)]
+        self.envs = [self.gym.make(self.name) for _ in range(self.n_agents)]
+        self.env = 0
+
         self.actions = self.envs[0].action_space.n
+
+        os.system("toilet {}".format(self.name))
+
+    def weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            torch.nn.init.Xavier(m.weight.data, 0.0, 0.05)
+            torch.nn.fill(m.weight.bias, 1.0)
+        if classname.find('Conv2d') != -1:
+            torch.nn.init.Xavier(m.weight.data, 1.0, 0.05)
+            torch.nn.fill(m.weight.bias, 0.0)
 
     def set_agent(self, agent, lr):
         self.agent = agent
-        self.optimizer = self.optim(lr=lr, params=self.agent.parameters())
+        self.weights_init(self.agent)
+        self.target_agent = copy.copy(agent)
+        print(self.agent)
+
+        if lr > 0.0:
+            self.optimizer = self.optim(lr=lr, weight_decay=0.99, params=self.target_agent.parameters())
         self.memory.set_keys(agent.transition_keys)
 
-    def preprocess_frame(self, state):
-        state = torch.tensor(state).float()
+    def set_rate(self, lr):
+        print("Learning rate: {}".format(lr))
+        self.optimizer = self.optim(lr=lr, params=self.agent.parameters())
 
-        if self.preview:
-            cv2.imshow("prev", state.np())
-            cv2.waitKey(1)
-        if self.norm:
-            mean = state.mean()
-            std = state.std()
-            state = (state - mean) / (std + 1e-10)
-        return state
 
     def replay_episode(self, episode):
         for t in episode:
@@ -74,11 +108,17 @@ class Coach():
         states = []
         for env in self.envs:
             state=env.reset()
+            for _ in range(self.init_skip):
+                state,_,_,info = env.step(0)
             states.append(state)
 
+        if "ale.lives" in info.keys():
+            self.life = info["ale.lives"]
+            self.life = self.life
         return states
 
     def end(self):
+        print("\nClosing gym")
         [env.close() for env in self.envs]
 
     #environment step
@@ -87,7 +127,6 @@ class Coach():
         rewards = []
         dones = []
         infos = []
-
         #take step for each agent/environment
         for i,env in enumerate(self.envs):
             reward = 0.0
@@ -102,25 +141,6 @@ class Coach():
         #send each return as separate vecs
         return obs, rewards, dones, infos
 
-    #loss function for vanilla reinforce algorithm
-    def reinforce(self, gamma):
-        loss = []
-
-        #return list of episodes
-        #   episodes: list of dicts
-        for episode in self.memory.get_episodes():
-            logits = episode["logit"]
-            values = episode["value"]
-            entropies = episode["entropy"]
-            expectation = torch.stack(logits) * torch.tensor(values) + (0.001*torch.tensor(entropies))
-            loss.append(expectation.sum())
-        self.optimizer.zero_grad()
-
-        loss = torch.stack(loss).mean().backward(retain_graph=True)
-        self.optimizer.step()
-        self.memory.reset()
-        return loss
-
     def get_keys(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -130,12 +150,40 @@ class Coach():
                 key = pygame.key.name(event.key)
                 if key == 'v':
                     self.render = not self.render
-                    print("Render {}".format(self.render))
+                    #print("Render {}".format(self.render))
                 if key == 'r':
                     self.replay_episode(self.best_episode)
+                if key == 'f':
+                    if self.render_frameskip == 1:
+                        self.render_frameskip = 10
+                    else:
+                        self.render_frameskip = 1
+
+
+    def render_env(self):
+        for env in self.envs:
+            env.render()
+
+    def learn(self, gamma):
+        return self.update(gamma, self.memory, self.optimizer)
+
+    def target_net_update(self):
+        self.agent.load_state_dict(self.target_agent.state_dict())
 
     #generate single trajectory
-    def run_episode(self, render=False, gamma=0.9):
+    def run_episode(self, render=False, gamma=0.9, max_steps=100, explore=False):
+
+        if explore:
+            mode = "Explore"
+        else:
+            mode = "Exploit"
+
+        print("Running {} episode.".format(mode))
+        self.render = render
+
+        for env in self.envs:
+            env._max_episode_steps = max_steps
+
         self.last_episode = []
 
         state = self.reset()
@@ -143,39 +191,59 @@ class Coach():
         steps = np.zeros(self.n_agents)
         rewards = np.expand_dims(np.zeros(self.n_agents),axis=0)
         score = 0
+        confidence = 0.0
 
-        while True:
+        for _ in range(max_steps):
             self.get_keys()
 
-            #gets action/s and replay memory info
-            self.memory.add_value(("state",observation.view(-1)))
+            #gets action/s and replay memory infov
+            #self.memory.add_value(("state",observation.view(-1)))
 
-            action, memory_info = self.agent.step(observation, self.memory)
+            action, memory_info = self.agent.step(observation, self.memory, confidence, self.render, explore)
+
             obs, reward, done, info = self.step(action)
             score += reward[0]
 
-            self.memory.add_value(("action",action[0]))
+            if self.render:
+                self.render_env()
+                #print("Step {} Rewards {}".format(self.steps, score))
+
             observation = self.preprocess_frame(obs)
+
+            confidence *= self.caution
+            confidence += reward[0]
+            confidence = max(min(confidence, 1), 0.2)
+
+            self.memory.add_value(("action",action[0]))
 
             dones = [not d for d in done]
 
             steps += dones * np.ones(self.n_agents)
             rewards += np.array(dones) * np.array(reward[0])
 
-            if (self.render or render) and steps % 1 == 0:
-                [env.render() for env in self.envs]
-
             for i,r in enumerate(reward):
-                self.memory.add_value(("reward",reward[0]))
+                if r > 0.0:
+                    r = 1.0
+                if r < 0.0:
+                    r = -1.0
 
-            if np.prod(done, 0):
-                #when episode is finished
+                self.memory.add_value(("reward",r))
+
+            if info[0]["ale.lives"] < self.life or np.prod(done, 0):
+                self.confidence = 0.0
                 self.memory.finish_episode(gamma)
+                self.life -= 1
+                states = []
+                for env in self.envs:
+                    for _ in range(self.mid_skip):
+                        state, _, _, info = env.step(0)
+                    states.append(state)
                 break
 
         #after series of episode, get episodes into own arrays
+        self.agent.steps = 0
         return int(score), int(steps)
-
+        torch.tensor()
     def discount(self, gamma):
         R = 0.0
 
